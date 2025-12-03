4680b38c27573ca3fed197a473ca4e917390dd8d
vicky <jvitoclucero@gmail.com>
2025-12-03 21:25:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-from bot import run_discord_bot
+import threading
+from bot import run_discord_bot, bot
+from server import start_webserver
+
+# Start FastAPI web server in another thread
+web_thread = threading.Thread(
+    target=start_webserver,
+    args=(bot,),
+    daemon=True
+)
+web_thread.start()
+
+# Start Discord bot (blocking)
 run_discord_bot()
